7811636917f637e344c5c096a05cb5693c9e956c
kimseongjae <stwin755@gmail.com>
2024-01-26 16:20:19 +0900

    controller main

---
+++ src/main/java/org/example/exercisecv/controller/BoardController.java
@@ -0,0 +1,12 @@
+package org.example.exercisecv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+    @GetMapping("/main")
+    public String main() {
+        return "main";
+    }
+}
