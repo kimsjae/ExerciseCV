9a913c4bea276a0f1021cf21d3d64ff779f81999
kimseongjae <stwin755@gmail.com>
2024-01-26 16:18:03 +0900

    controller join

--- src/main/java/org/example/exercisecv/controller/UserController.java
+++ src/main/java/org/example/exercisecv/controller/UserController.java
@@ -2,11 +2,28 @@ package org.example.exercisecv.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
     @GetMapping("/joinForm")
     public String joinForm() {
         return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(String username, String password, String email) {
+        System.out.println("username : " + username);
+        System.out.println("password : " + password);
+        System.out.println("email : " + email);
+
+        if (username.length() > 16 || password.length() > 16) {
+            return "error-404";
+        }
+        if (username == null || password == null || email == null) {
+            return "error-404";
+        }
+
+        return "redirect:/main";
     }
 }
