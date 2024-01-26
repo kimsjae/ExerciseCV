90739fa4d12701f35ec98d9664e6aa81883f3d8f
kimseongjae <stwin755@gmail.com>
2024-01-26 15:56:28 +0900

    init

---
+++ src/main/java/org/example/exercisecv/ExerciseCvApplication.java
@@ -0,0 +1,13 @@
+package org.example.exercisecv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseCvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/exercisecv/ExerciseCvApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exercisecv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
